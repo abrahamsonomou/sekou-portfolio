73b502487a1253bb1bfdbf6b22114bebfb7eb9c9
Sékou SONOMOU <sekou.sonomou@ipnetinstitute.com>
2022-06-24 16:39:26 +0000

    Update ALLOWED_HOSTS with site and development server URL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-g7qxrr5^1j$793rm1+$*o!mbzz#b*5(2fjkxl1j#s%szz!ew*7'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-g7qxrr5^1j$793rm1+$*o!mbzz#b*5(2fjkxl1j#s%szz!ew*7')
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-g7qxrr5^1j$793rm1+$*o!mbzz#b*5(2fjkxl1j#s%szz!ew*7')
 
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
