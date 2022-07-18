d35773df6d8f406870790b2efcb7d4a130595242
Sékou SONOMOU <sekou.sonomou@ipnetinstitute.com>
2022-07-18 14:36:59 +0000

    change settings

--- app_web/forms.py
+++ app_web/forms.py
@@ -10,7 +10,7 @@ class ContactForm(forms.ModelForm):
         labels={'nom':'Full Name','email':'Email Address','sujet':'Sujet','message':'Message'}
         widgets={
             'nom':forms.TextInput(attrs={'class':'form-control','placeholder':"Nom",'required':'required',}),
-            'email':forms.TextInput(attrs={'class':'form-control','placeholder':"Email",'required':'required',}),
+            'email':forms.EmailInput(attrs={'class':'form-control','placeholder':"Email",'required':'required',}),
             'sujet':forms.TextInput(attrs={'class':'form-control','placeholder':"Sujet",'required':'required',}),
             'message':forms.Textarea(attrs={'class':'form-control','cols':30,'rows':10,'placeholder':"Message",'required':'required',})
         }
