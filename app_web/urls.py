1a0252f749c44d943e8989391e115c87b04f8dbb
Sékou SONOMOU <sekou.sonomou@ipnetinstitute.com>
2022-07-18 13:26:24 +0000

    change settings

--- app_web/urls.py
+++ app_web/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('',index,name='index'),
+    path('',index,name='index'),
     path('projet/<slug>/',detail_projet,name='detail_projet'),
-    path('',Contact.as_view(),name='index')
-    # path('contact',Contact.as_view(),name='contact')
+    path('contact',Contact.as_view(),name='contact')
     # path('contact',contact,name='contact')
 ]
